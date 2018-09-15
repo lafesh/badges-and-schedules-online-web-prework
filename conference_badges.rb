80d3b174238a5878fe79697edb2981796bc25cc9
Gina Fedder <ginafedder@yahoo.com>
2018-09-15 22:46:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(attendees)
 end 
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+  batch_badge_creator(attendees).each {|list| puts list}
   puts assign_rooms(attendees)
   
 end
